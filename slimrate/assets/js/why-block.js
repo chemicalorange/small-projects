0aaa47307af6ad95c0f3504dbe98ffc33c178333
chemicalorange <66693973+chemicalorange@users.noreply.github.com>
2022-02-09 23:28:11 +0700

    ffff

--- slimrate/assets/js/why-block.js
+++ slimrate/assets/js/why-block.js
@@ -1,18 +1,19 @@
 const whyBlockAnimation = () => {
     let img = document.querySelector('.why-block__img')
     let listItems = document.querySelectorAll('.why-block__list li')
+    let list = document.querySelector('.why-block__list')
 
     listItems.forEach((listItem, index) => {
         listItem.addEventListener('mouseover', ()=>{
             img.classList.add(`animation-st-${index + 1}`)
+            list.classList.add(`animation-st-${index + 1}`)
             listItems.forEach((item, i)=>{
                 if(i != index) item.classList.add('unactive') 
             })
-
-
         })
         listItem.addEventListener('mouseout', ()=>{
             img.classList.remove(`animation-st-${index + 1}`)
+            list.classList.remove(`animation-st-${index + 1}`)
             listItems.forEach(item => item.classList.remove('unactive'))
         })
     })
